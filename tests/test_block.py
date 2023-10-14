36dcbf4c09f9e6fa2b981f07730eadc9c9a51c3a
tobiadefami <tobiadefami@gmail.com>
2023-10-15 00:32:33 +0100

    ADD: tests

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,4 +1,38 @@
-from llmagic.block import Block
+from llmagic.block import Block, TextBlock
+from llmagic.tokenizer import tokenizer
+from rich.panel import Panel
+
+
+def test_create_block_with_text():
+    block = Block(text= "this is a sample text", max_tokens=1024)
+    assert block.full_text() == "this is a sample text"
+
+def test_add_text_to_block():
+    block = Block(text="this is", max_tokens=1024)
+    block += TextBlock(" a sample text")
+    assert block.full_text() == "this is a sample text"
+
+def test_create_children_with_block():
+    child_block = Block(text="this is a child block")
+    block = Block(children=[child_block])
+    assert block.full_text() == "this is a child block"
+
+def test_block_full_size():
+    block = Block(text="this is a sample text")
+    assert block.full_size() == len(tokenizer.encode(block.full_text()))
+    assert block.size() == len(tokenizer.encode(block.full_text()))
+
+def test_rich_text_method():
+    block = Block(text="this is a sample text")
+    rich_panel = block.rich_text(max_tokens=10)
+    assert isinstance(rich_panel, Panel)        
+
+def test_append_method():
+    block = Block(text= 'this is a sample text')
+    block.append(", append a given text to it")
+    assert block.full_text() == 'this is a sample text, and i am going to append a given text to it'
+
+
 
 
 def test_block():
@@ -49,3 +83,4 @@ def test_block_hierarchy():
     context += "This is another prompt that's a bit longer"
     # First portion of context gets truncated
     assert context.text() == "This is another prompt that's a bit longer"
+
