eb3ab321d081091cd2a99a324cbfdb2301a86021
tobiadefami <tobiadefami@gmail.com>
2023-11-01 04:13:43 +0100

    probably working correctly

--- llmagic/boundary.py
+++ llmagic/boundary.py
@@ -1,17 +1,46 @@
 from llmagic.dtypes import Boundary
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 
-def find_boundary_points(encoding, tokenizer, boundary: Boundary):
+def find_boundary_points(encoding, tokenizer, boundary: Boundary) -> list[int]:
     """
     Return token indices that correspond to valid boundary points
     """
+    boundary_points: list = []
     if boundary == "token":
         return list(range(len(encoding.ids)))
     elif boundary == "line":
         boundary_token = tokenizer.encode("\n").ids[0]
-        boundary_points = [
+        boundary_points.extend([
             i for i, token in enumerate(encoding.ids) if token == boundary_token
-        ]
-        return boundary_points
+        ])
+    elif boundary == "whitespace":
+        whitespace_token = tokenizer.encode("\t").ids[0] or tokenizer.encode(" ").ids[0]
+        print("WHITESPACE TOKEN", whitespace_token)
+        print("ENCODING IDS", encoding.ids)
+        print("TEXT", tokenizer.decode(encoding.ids))
+
+        decode = tokenizer.decode(encoding.ids).split()
+        tok_map = {}
+        for ids, tok in zip(encoding.ids, decode):
+            tok_map[ids] = tok
+        print(tok_map)
+
+        boundary_points.extend([
+            i for i, token in enumerate(encoding.ids) if token == whitespace_token
+        ])
+
+    elif boundary == "sentence":
+        decoded_text = tokenizer.decode(encoding.ids)
+        doc = nlp(decoded_text)
+        for sent in doc.sents:
+            start = sent[0].i
+            end = sent[-1].i
+            boundary_points.append(start)
+            boundary_points.append(end)
+
     else:
         raise NotImplementedError(f"Boundary {boundary} not implemented")
+    return boundary_points
--- llmagic/truncation.py
+++ llmagic/truncation.py
@@ -13,57 +13,67 @@ def truncate_encoding(self, *args, **kwargs):
     return copied
 
 
+def add_ellipsis_token(tokens, ellipsis_token, direction='right'):
+    n_ellipsis_tokens = len(ellipsis_token.ids)
+    num_tokens_to_keep = max(0, len(tokens.ids) - n_ellipsis_tokens)
+    if num_tokens_to_keep > 0:
+
+        if direction == "left":
+            first_tokens_removed = truncate_encoding(
+                tokens, num_tokens_to_keep, direction="left"
+            )
+            tokens = Encoding.merge([ellipsis_token, first_tokens_removed])
+        elif direction == "right":
+            last_tokens_removed = truncate_encoding(
+                tokens, num_tokens_to_keep, direction="right"
+            )
+            tokens = Encoding.merge([last_tokens_removed, ellipsis_token])
+
+    return tokens
+
+
+def process_boundary_points(boundary_points: list[int], max_tokens: int, size: int = None, direction="right") -> int:
+    if boundary_points is not None:
+
+        if direction == "left":
+            while (size - max_tokens - 1) not in boundary_points and (size - max_tokens - 1) > 0:
+                max_tokens -= 1
+        elif direction == "right":
+            while max_tokens not in boundary_points and max_tokens > 0:
+                max_tokens -= 1
+
+    return max_tokens
+
+
 def truncate(
-    tokens: Encoding,
-    max_tokens: int | None,
-    truncation_strategy: TruncationStrategy,
-    tokenizer: Callable,
-    ellipsis: bool = False,
-    boundary_points: list[int] | None = None,
+        tokens: Encoding,
+        max_tokens: int | None,
+        truncation_strategy: TruncationStrategy,
+        tokenizer: Callable,
+        ellipsis: bool = False,
+        boundary_points: list[int] | None = None,
 ) -> dict[str, Encoding]:
     size = len(tokens.ids)
     remainder_right = Encoding()
     remainder_left = Encoding()
     ellipsis_tokens: Encoding = tokenizer.encode("...")
-    n_ellipsis_tokens: int = len(ellipsis_tokens.ids)
+    # n_ellipsis_tokens: int = len(ellipsis_tokens.ids)
     if max_tokens is not None and size > max_tokens:
         match truncation_strategy:
             case "right":
-                # Factor this out into a function
-                if boundary_points is not None:
-                    while max_tokens not in boundary_points and max_tokens > 0:
-                        max_tokens -= 1
-
-                cutoff = size - max_tokens
+                processed_max_tokens = process_boundary_points(boundary_points, max_tokens, direction="right")
+                cutoff = size - processed_max_tokens
                 remainder_right = truncate_encoding(tokens, cutoff, direction="left")
-                tokens = truncate_encoding(tokens, max_tokens, direction="right")
+                tokens = truncate_encoding(tokens, processed_max_tokens, direction="right")
                 if ellipsis:
-                    # Factor out
-                    num_tokens_to_keep = max(0, len(tokens.ids) - n_ellipsis_tokens)
-                    if num_tokens_to_keep > 0:
-                        last_tokens_removed = truncate_encoding(
-                            tokens, num_tokens_to_keep, direction="right"
-                        )
-                        tokens = Encoding.merge([last_tokens_removed, ellipsis_tokens])
+                    tokens = add_ellipsis_token(tokens, ellipsis_token=ellipsis_tokens, direction="right")
             case "left":
-                # Factor out
-                if boundary_points is not None:
-                    while (size - max_tokens - 1) not in boundary_points and (
-                        size - max_tokens - 1
-                    ) > 0:
-                        max_tokens -= 1
-
-                cutoff = size - max_tokens
+                processed_max_tokens = process_boundary_points(boundary_points, max_tokens, size, direction="left")
+                cutoff = size - processed_max_tokens
                 remainder_left = truncate_encoding(tokens, cutoff, direction="right")
-                tokens = truncate_encoding(tokens, max_tokens, direction="left")
+                tokens = truncate_encoding(tokens, processed_max_tokens, direction="left")
                 if ellipsis:
-                    # Factor out
-                    num_tokens_to_keep = max(0, len(tokens.ids) - n_ellipsis_tokens)
-                    if num_tokens_to_keep > 0:
-                        first_tokens_removed = truncate_encoding(
-                            tokens, num_tokens_to_keep, direction="left"
-                        )
-                        tokens = Encoding.merge([ellipsis_tokens, first_tokens_removed])
+                    tokens = add_ellipsis_token(tokens, ellipsis_token=ellipsis_tokens, direction="left")
             case _:
                 # No truncation
                 pass
