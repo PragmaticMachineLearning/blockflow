a20dc6d0df090e22c0705a72ec1bfe2d436dbfa5
tobiadefami <tobiadefami@gmail.com>
2024-01-10 00:01:51 +0100

    FIX: warnings

--- llmagic/config.py
+++ llmagic/config.py
@@ -1,24 +1,10 @@
-
-# class Config:
-#     def __init__(self):
-#         self.boundary_name = None
-
-#     @property
-#     def set_boundary_name(self, name):
-#         print(f"Setting boundary name {name}")
-#         self.boundary_name = name
-    
-                
-#     def get_boundary_name(self):
-#         print(f"Getting boundary name {self.boundary}")
-#         self.boundary_name
-
 CONFIG = {}
 
 
 def set_boundary_name(name):
     global CONFIG
-    CONFIG['boundary_name'] = name
-    
+    CONFIG["boundary_name"] = name
+
+
 def get_boundary_name():
-    return CONFIG.get('boundary_name', None)
+    return CONFIG.get("boundary_name", None)
